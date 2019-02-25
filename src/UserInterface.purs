74866cec1704bec65dec33e0140885d976645d52
redxaxder <abyaly@gmail.com>
2019-02-24 20:09:49 -0600

    added stub for inventory ui

--- src/UserInterface.purs
+++ src/UserInterface.purs
@@ -13,20 +13,33 @@ import Intent (Action (..))
 import Direction (Direction (..))
 
 
+
+-- Javascript key codes here: https://keycode.info/
+
 type Key = String
-
 data UIRenderData = UIRenderData
-
 data UI = AwaitingInput UIRenderData (Key -> GameState -> UI) | GameAction Action UI
 
 uiInit :: UI
 uiInit = move
 
+showMove :: UIRenderData
+showMove = UIRenderData
+
 move :: UI
-move = AwaitingInput UIRenderData go
+move = AwaitingInput showMove go
   where
     go "ArrowLeft"  _ = GameAction (Move Left) move
     go "ArrowRight" _ = GameAction (Move Right) move
     go "ArrowDown"  _ = GameAction (Move Down) move
     go "ArrowUp"    _ = GameAction (Move Up) move
-    go _ _            = move
+    go "KeyI"       _ = inventory
+    go _            _ = move
+
+showInventory :: UIRenderData
+showInventory = UIRenderData
+
+inventory :: UI
+inventory = AwaitingInput showInventory go
+  where
+    go _ _ = todo
