18da6677e87e034ac693915eb3ef41b8ec918150
redxaxder <abyaly@gmail.com>
2019-03-05 17:31:50 -0600

    Toward map gen!

--- src/MapGen.purs
+++ src/MapGen.purs
@@ -5,11 +5,11 @@ import Extra.Prelude
 import Data.Array (unsnoc, cons, concat, catMaybes, zipWith)
 import Data.Foldable (find)
 import Data.FunctorWithIndex (mapWithIndex)
-import Data.String.CodeUnits (fromCharArray, toCharArray)
+import Data.String.CodeUnits (toCharArray)
 
 import Atlas (Atlas, Chart, LocalPosition, Position(..), addStitch, ChartId, mkChart, addChart, mkAtlas)
 import Direction (Direction(..))
-import Direction (add) as Dir
+import Direction as Dir
 import Random (Gen)
 import Tile (Tile(..))
 import Types (MapGenHint, Placeholder)
@@ -25,33 +25,26 @@ genMapPiece p@{ position, direction } hint atlas =
       atlas'' = addStitch position direction entrancePosition atlas'
    in { atlas: atlas'', placeholders}
 
-
 rotateLeft :: forall a. Array (Array a) -> Array (Array a)
 rotateLeft xs = case sequence $ map unsnoc xs of
   Just pairs -> cons (map (_.last) pairs) (rotateLeft (map (_.init) pairs))
   Nothing -> mempty
-  
-rotate :: (Array String) -> Direction -> Array String
-rotate ar U = map fromCharArray $ rotateLeft (map toCharArray ar)
-rotate ar L = map fromCharArray $ rotateLeft $ rotateLeft (map toCharArray ar)
-rotate ar D = map fromCharArray $ rotateLeft $ rotateLeft $ rotateLeft (map toCharArray ar)
-rotate ar _ = ar
+
+rotate :: forall a. Direction -> Array (Array a) -> Array (Array a)
+rotate d = repeatedly (Dir.toInt d) rotateLeft
 
 type MapData = { terrain :: Array String, next :: Array MapGenHint }
 
 initMap :: Gen -> { atlas :: Atlas Tile, player :: Position }
-initMap g = 
+initMap g =
   let { terrain, next } = startRoom { rng: g }
-      { chart, exits } = load next terrain D
-      errorRoom = mkChart Wall [[Wall]] 
+      { chart, exits } = load next terrain R
+      errorRoom = mkChart Wall [[Wall]]
       atlasZero = mkAtlas errorRoom
       Tuple chartId atlas = addChart chart atlasZero
   in  { atlas
       , player: Position { chartId, localPosition: V {x: 1,y: 1} }
       }
-
-
-
 
 load
   :: Array MapGenHint
@@ -62,7 +55,7 @@ load
      , entrance :: LocalPosition
      }
 load hints rows rotation =
-  let mapTokens = toMapTokens (rotate rows rotation)
+  let mapTokens = rotate rotation $ toMapTokens rows
       indexedMap = addIndices mapTokens
       tiles = (map <<< map) getTile mapTokens
       protoExits = catMaybes $ flip map (indexedMap) $ \{ x, y, a } ->
@@ -141,7 +134,7 @@ sampleMap1 {rng} =
       ]
   , next: [{rng},{rng},{rng}]
   }
-  
+
 sampleHall1 :: MapGenHint -> MapData
 sampleHall1 {rng} = 
   { terrain: 
