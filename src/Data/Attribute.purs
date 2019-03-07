89f5af60c61029ecd844a8728db975ce9b035966
redxaxder <abyaly@gmail.com>
2019-03-06 21:51:14 -0600

    refactor in preparation for kitchen display

---
+++ src/Data/Attribute.purs
@@ -0,0 +1,7 @@
+module Data.Attribute where
+
+import Extra.Prelude
+
+newtype Attribute = Attribute String
+derive instance eqAttribute :: Eq Attribute
+derive instance newtypeAttribute :: Newtype Attribute _
--- src/Types.purs
+++ src/Types.purs
@@ -1,13 +1,27 @@
-module Types where
+module Types
+  ( module Types
+  , module Data.Attribute
+  , module Data.Furniture
+  , module Data.Item
+  , module Data.Region
+  , module Data.Sprite
+  , module Data.Tile
+  )
+  where
 
 import Extra.Prelude
 
 import Data.Map (Map)
 
 import Atlas (Atlas, Position)
-import Tile (Tile)
-import Random (Gen)
+import Data.Attribute (Attribute (..))
+import Data.Furniture (Furniture (..), FurnitureName (..))
+import Data.Item (Item (..), ItemName (..))
+import Data.Region (Region (..))
+import Data.Sprite (Sprite (..))
+import Data.Tile (Tile (..))
 import Direction (Direction)
+import Random (Gen)
 
 type GameState =
  { player :: Position
@@ -31,36 +45,3 @@ type Placeholder = { position :: Position, direction :: Direction, next :: MapGe
 type FieldOfView = Array { screen :: Vector Int, absolute :: Position, tiles :: Array Tile }
 
 type MapData = { terrain :: Array String }
-
-
-newtype Sprite = Sprite { offsetX :: Int, offsetY :: Int }
-
-newtype Attribute = Attribute String
-derive instance eqAttribute :: Eq Attribute
-
-newtype ItemName = ItemName String
-derive instance eqItemName :: Eq ItemName
-derive instance ordItemName :: Ord ItemName
-derive instance newtypeItemName :: Newtype ItemName _
-
-newtype Item = Item
-  { name :: ItemName
-  , char :: Maybe Char
-  , sprite :: Sprite
-  , attributes :: Array Attribute
-  }
-
-derive instance newtypeItem :: Newtype Item _
-
-newtype FurnitureName = FurnitureName String
-derive instance eqFurnitureName :: Eq FurnitureName
-derive instance ordFurnitureName :: Ord FurnitureName
-derive instance newtypeFurnitureName :: Newtype FurnitureName _
-
-newtype Furniture = Furniture
-  { name :: FurnitureName
-  , char :: Char
-  , sprite :: Sprite
-  , attributes :: Array Attribute
-  }
-derive instance newtypeFurniture :: Newtype Furniture _
